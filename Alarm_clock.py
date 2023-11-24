32fc3074be0a9177b48557aacc7242dc50070046
Jakiur Rahman <jakiurrahman001@gmail.com>
2023-11-24 20:16:21 +0530

    Code enhancement

--- Alarm_clock.py
+++ Alarm_clock.py
@@ -1,88 +1,63 @@
-# Import Required Library
 from tkinter import *
 import datetime
-import time
-import winsound
-from threading import *
+import simpleaudio as sa
+import threading
 
-# Create Object
+def set_alarm():
+    set_alarm_time = f"{hour.get()}:{minute.get()}:{second.get()}"
+    alarm_triggered = False
+    
+    while True:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        if current_time == set_alarm_time and not alarm_triggered:
+            alarm_message()
+            alarm_triggered = True
+        elif current_time != set_alarm_time:
+            alarm_triggered = False
+
+def alarm_message():
+    print("Time to Wake up")
+    # Load and play the sound file
+    wave_obj = sa.WaveObject.from_wave_file("sound.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()  # Wait for the sound to finish playing
 
 root = Tk()
-
-# Set geometry
 root.geometry("400x200")
+root.title("Alarm Clock")
 
-# Use Threading
-
-def Threading():
-	t1=Thread(target=alarm)
-	t1.start()
-
-def alarm():
-	# Infinite Loop
-	while True:
-		# Set Alarm
-		set_alarm_time = f"{hour.get()}:{minute.get()}:{second.get()}"
-
-		# Wait for one seconds
-		time.sleep(1)
-
-		# Get current time
-		current_time = datetime.datetime.now().strftime("%H:%M:%S")
-		print(current_time,set_alarm_time)
-
-		# Check whether set alarm is equal to current time or not
-		if current_time == set_alarm_time:
-			print("Time to Wake up")
-			# Playing sound
-			winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
-
-# Add Labels, Frame, Button, Optionmenus
-Label(root,text="Alarm Clock",font=("Helvetica 20 bold"),fg="red").pack(pady=10)
-Label(root,text="Set Time",font=("Helvetica 15 bold")).pack()
+Label(root, text="Alarm Clock", font=("Helvetica", 20, "bold"), fg="red").pack(pady=10)
+Label(root, text="Set Time", font=("Helvetica", 15, "bold")).pack()
 
 frame = Frame(root)
 frame.pack()
 
 hour = StringVar(root)
-hours = ('00', '01', '02', '03', '04', '05', '06', '07',
-		'08', '09', '10', '11', '12', '13', '14', '15',
-		'16', '17', '18', '19', '20', '21', '22', '23', '24'
-		)
+hours = [str(i).zfill(2) for i in range(25)]
 hour.set(hours[0])
 
 hrs = OptionMenu(frame, hour, *hours)
 hrs.pack(side=LEFT)
 
 minute = StringVar(root)
-minutes = ('00', '01', '02', '03', '04', '05', '06', '07',
-		'08', '09', '10', '11', '12', '13', '14', '15',
-		'16', '17', '18', '19', '20', '21', '22', '23',
-		'24', '25', '26', '27', '28', '29', '30', '31',
-		'32', '33', '34', '35', '36', '37', '38', '39',
-		'40', '41', '42', '43', '44', '45', '46', '47',
-		'48', '49', '50', '51', '52', '53', '54', '55',
-		'56', '57', '58', '59', '60')
+minutes = [str(i).zfill(2) for i in range(61)]
 minute.set(minutes[0])
 
 mins = OptionMenu(frame, minute, *minutes)
 mins.pack(side=LEFT)
 
 second = StringVar(root)
-seconds = ('00', '01', '02', '03', '04', '05', '06', '07',
-		'08', '09', '10', '11', '12', '13', '14', '15',
-		'16', '17', '18', '19', '20', '21', '22', '23',
-		'24', '25', '26', '27', '28', '29', '30', '31',
-		'32', '33', '34', '35', '36', '37', '38', '39',
-		'40', '41', '42', '43', '44', '45', '46', '47',
-		'48', '49', '50', '51', '52', '53', '54', '55',
-		'56', '57', '58', '59', '60')
+seconds = [str(i).zfill(2) for i in range(61)]
 second.set(seconds[0])
 
 secs = OptionMenu(frame, second, *seconds)
 secs.pack(side=LEFT)
 
-Button(root,text="Set Alarm",font=("Helvetica 15"),command=Threading).pack(pady=20)
+def set_alarm_thread():
+    t = threading.Thread(target=set_alarm)
+    t.daemon = True
+    t.start()
 
-# Execute Tkinter
+Button(root, text="Set Alarm", font=("Helvetica", 15), command=set_alarm_thread).pack(pady=20)
+
 root.mainloop()
